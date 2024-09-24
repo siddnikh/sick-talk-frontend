a06a49e6201ef5c1c170eaf515bc4baff20c9c01
siddnikh <mailtosiddnikh@gmail.com>
2024-09-24 17:48:08 +0530

    half-assed attempt to make this a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SICKTALK',
+        short_name: 'sicktalk',
+        description: 'Ain\'t other place to talk than this.',
+        theme_color: '#FDB1D7',
+        icons: [
+          {
+            src: 'logo.png',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
